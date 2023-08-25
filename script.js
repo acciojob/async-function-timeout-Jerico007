7ba88d3b55c129ff9e80912eb475f1b8dfe4a8bb
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-08-25 12:26:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+let text = "";
+let delay = 0;
+
+let	btn  = document.getElementById("btn");
+ btn.addEventListener("click" , async function Resolve(){
+	text  = document.getElementById("text").value;
+	
+	delay = Number(document.getElementById("delay").value);
+	 document.getElementById("output").innerHTML = "";
+	let ans = await getMessage();
+	 document.getElementById("output").innerHTML = ans;
+});
+
+
+function getMessage(){
+	let prom = new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(text);
+		},delay*1000);
+	})
+
+  let x = prom.then((data)=>{
+		return data;
+	});
+
+	return x;
+}
+
+
